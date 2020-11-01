9d65ce1ed8afbf261d5616b60b470cd3bd365a6d
Giedrius Backaitis <giedriusbackaitis@gmail.com>
2020-11-01 22:08:44 +0200

    Encypher and decypher functionalities

---
+++ CaesarsCipher/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Text;
+
+namespace CaesarsCipher
+{
+    class Program
+    {
+        private const int ALPHABET_SIZE = 26;
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            string textPrintMessage = "Hello, please type the text you would like to encrypt/decrypt(Please use letters A-Z): ";
+            string actionPrintMessage = "Please choose the action you would like to perform from the options below:\n" +
+                "(To encrypt/decrypt text, please type \"encrypt/decrypt\" ";
+            string keyPrintMessage = "Please enter the key for encryption/decryption: ";
+            string wrongKeyPrintMessage = "The key you entered is invalid. Please try again";
+            string wrongActionPrintMessage = "Entered text is not a valid action. Please try again";
+
+            Phase1:
+            Console.WriteLine(textPrintMessage);
+            string text = Console.ReadLine();
+            Console.WriteLine(keyPrintMessage);
+            bool isInt = int.TryParse(Console.ReadLine(), out int key);
+            if (!isInt)
+            {
+                Console.WriteLine(wrongKeyPrintMessage);
+                goto Phase1;
+            }
+            Console.WriteLine(actionPrintMessage);
+            string action = Console.ReadLine();
+            string result;
+            switch (action)
+            {
+                case("encrypt"):
+                    result = Encipher(text, key);
+                    break;
+                    
+                case ("decrypt"):
+                    result = Decipher(text, key);
+                    break;
+                    
+                default:
+                    Console.WriteLine(wrongActionPrintMessage);
+                    goto Phase1;
+            }
+            Console.WriteLine("Result is: " + result);
+           
+        }
+
+        private static string Decipher(string text, int key)
+        {
+            return Encipher(text, -key);
+        }
+
+        private static string Encipher(string text, int key)
+        {
+            key %= ALPHABET_SIZE;
+            StringBuilder encipheredText = new StringBuilder();
+            foreach (char letter in text)
+            {
+                if (char.IsLower(letter))
+                {
+                    encipheredText.Append(GetLetter(letter, key, 'a', 'z'));
+                }
+                else if (char.IsUpper(letter))
+                {
+                    encipheredText.Append(GetLetter(letter, key, 'A', 'Z'));
+                }
+            }
+            return encipheredText.ToString();
+        }
+        
+
+        private static char GetLetter(char letter, int key, char lowest, char highest)
+        {
+            letter += (char)key;
+            if (letter < lowest)
+            {
+                return (char)(letter + ALPHABET_SIZE);
+            }
+            if (letter > highest)
+            {
+                return (char)(letter - ALPHABET_SIZE);
+            }
+            return letter;
+        }
+
+    }
+}
